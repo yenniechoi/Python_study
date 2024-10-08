97e2a7c09cac5c8f1b8063b45389d56198a2446f
yenniechoi <choiyeah123@gmail.com>
2024-10-08 21:03:41 +0900

    xor(^), [::2], enumerate(), pow(), set(), math.prod()

--- Programmers_Basic/day_5.py
+++ Programmers_Basic/day_5.py
@@ -1,10 +1,11 @@
 '''
 21) 코드 처리하기
-22)
+22) 등차수열의 특정한 항만 더하기
 23) 주사위 게임 2
-24)
-25)
+24) 원소들의 곱과 합
+25) 이어 붙인 수
 '''
+
 
 
 # 21)
@@ -27,13 +28,13 @@ def turn_mode(code):
     return ret if ret else "EMPTY"
 
 '''
-# default strt(0), default end(len), step size of 2
+# default start(0), default end(len), step size of 2
 return "".join(code.split("1"))[::2] or "EMPTY"
 
 # xor 연산. 서로 같으면 0, 다르면 1
 mode ^= 1
 '''
-print(turn_mode("abc1abc1abc"))
+print(turn_mode("abc1abc1abc"))     # acbac
 
 
 # 22)
@@ -47,6 +48,7 @@ def arithmetic_sequence(a, d, included):
 '''
 return sum(a + i * d for i, f in enumerate(included) if f)
 '''
+# 37
 print(arithmetic_sequence(3, 4, [True, False, False, True, True]))
 
 
@@ -78,9 +80,40 @@ for i in range(4 - unique_count):
     answer *= (a ** (i + 1) + b ** (i + 1) + c ** (i + 1))
 '''
 
-print(three_dices(2, 6, 1))
-print(three_dices(5, 3, 3))
-print(three_dices(4, 4, 4))
+print(three_dices(2, 6, 1))     # 9
+#print(three_dices(5, 3, 3))
+#print(three_dices(4, 4, 4))
+
 
 # 24)
+import math
+def multiply_plus(num_list):
+    return int(math.prod(num_list) < sum(num_list) ** 2)
+
+'''
+def compare_product_sum(num_list):
+    product = 1
+    total_sum = 0
+
+    for num in num_list:
+        product *= num
+        total_sum += num
+
+    return int(product < total_sum ** 2)
+'''
+
+print(multiply_plus([3, 4, 5, 2, 1]))       # 1
+
+
 # 25)
+# 홀수만 이어붙임 + 짝수만 이어붙임
+def concatenate_num(num_list):
+    odd, even = '', ''
+    for num in num_list:
+        if num % 2 == 0:
+            even += str(num)
+        else:
+            odd += str(num)
+    return int(even) + int(odd)
+
+print(concatenate_num([5, 7, 8, 3]))        # 581
