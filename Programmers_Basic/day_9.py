a5939ff5cad13d7ae9d90f624094447e86903dec
yenniechoi <choiyeah123@gmail.com>
2024-10-27 23:10:37 +0900

    [-n:], sort(), sorted(), in, endswith()

--- Programmers_Basic/day_9.py
+++ Programmers_Basic/day_9.py
@@ -37,24 +37,44 @@ print(assemble_str(["progressive", "hamburger", "hammer", "ahocorasick"], [[0, 4
 
 # 43)
 def return_str_n(my_string, n):
-    answer = ''
-    return answer
+    return my_string[len(my_string)- n : len(my_string) + 1]
 
+'''
+return my_string[-n:]
+'''
 
 print(return_str_n("ProgrammerS123", 11))   # "grammerS123"
 print(return_str_n("He110W0r1d", 5))        # "W0r1d"
 
 
 # 44)
+# 모든 접미사를 사전순으로 정렬한 문자열 배열
+def every_suffix(my_string):
+    answer = []
+    for i in range(len(my_string)):
+        answer.append(my_string[-i:])
+    answer.sort()
+    return answer
 
+'''
+return sorted(my_string[i:] for i in range(len(my_string)))
+'''
 
-print()
+# ["a", "ana", "anana", "banana", "na", "nana"]
+print(every_suffix("banana"))
 
 
 # 45)
+def check_suffix(my_string, is_suffix):
+    return int(is_suffix in list((my_string[-i:] for i in range(len(my_string)))))
 
+'''
+return int(my_string[-len(is_suffix):] == is_suffix)
 
-print()
+return int(my_string.endswith(is_suffix))
+'''
+
+print(check_suffix("banana","ana"))     # 1
 
 
 
