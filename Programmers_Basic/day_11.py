d08bd447436cc722e583f385a884fb31afbd57f4
yenniechoi <choiyeah123@gmail.com>
2024-10-31 21:40:10 +0900

    ord(), not in, index(val, start)

--- Programmers_Basic/day_11.py
+++ Programmers_Basic/day_11.py
@@ -54,21 +54,56 @@ print(make_arr(10,	3))     # [3, 6, 9]
 
 # 53) ===========================================================================
 
+def remove_char(my_string, indices):
+    answer = list(my_string)
 
+    for index in indices:
+        answer[index] = ''
 
-print()
+    return ''.join(answer)
+
+'''
+# 인덱스 없는 글자를 이어붙임
+return "".join([v for i,v in enumerate(my_string) if i not in indices])
+
+# 인덱스를 내림차순 정렬 후 뒤에서부터 리스트 값 삭제
+my_string = list(my_string)
+for i in sorted(indices, reverse=True):
+    del my_string[i]
+return ''.join(my_string)
+'''
+
+# "programmers"
+print(remove_char("apporoograpemmemprs",	[1, 16, 6, 15, 0, 10, 11, 3]))
 
 
 # 54) ===========================================================================
+# start_num에서 end_num까지 1씩 감소하는 수
+def countdown(start_num, end_num):
+    return list(range(start_num, end_num-1, -1))
 
-
-
-print()
+print(countdown(10, 3))     # [10, 9, 8, 7, 6, 5, 4, 3]
 
 
 # 55) ===========================================================================
 
+def near_one(arr, idx):
+    for i in range(idx, len(arr)):
+        if arr[i] == 1:
+            return i
+    return -1
+
+'''
+answer = 0
+try:
+    answer = arr.index(1, idx)
+except:
+    answer = -1
+return answer
+'''
 
 
-print()
+print(near_one([0, 0, 0, 1], 1))            # 3
+print(near_one([1, 0, 0, 1, 0, 0],	4))     # -1
+print(near_one([1, 1, 1, 1, 0],	3))         # 3
 
