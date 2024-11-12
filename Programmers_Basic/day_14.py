58cc42d234f5f9f73a3e1a5d3781c0b0fa7a69c2
yenniechoi <choiyeah123@gmail.com>
2024-11-12 22:30:24 +0900

    sum(), list comprehension - if, next(), accumulate()

--- Programmers_Basic/day_14.py
+++ Programmers_Basic/day_14.py
@@ -45,22 +45,70 @@ print(group_leader(["nami", "ahri", "jayce", "garen", "ivern", "vex", "jinx"]))
 
 
 # 68) ====================================================================
+#  아직 마치지 못한 일들을 순서대로
 
+def todo(todo_list, finished):
+    return [t for t, r in zip(todo_list, finished) if not bool(r)]
 
+'''
+return [work for idx, work in enumerate(todo_list) if not finished[idx]]
 
-print()
+return [x for x, b in zip(todo_list, finished) if not b]
+'''
+'''
+JS
+return todo_list.filter((e,i) => !finished[i]);
+
+Java
+for(int i=0; i<finished.length; i++){
+    str = finished[i]==false ? str+todo_list[i]+"," : str;
+}
+return str.split(",");
+'''
+
+# ["practiceguitar", "studygraph"]
+print(todo(["problemsolving", "practiceguitar", "swim", "studygraph"],	[True, False, True, False]))
 
 
 # 69) ====================================================================
+# 앞에서부터 하나씩 더하다가 그 합이 n보다 커지는 순간 리턴
 
+def add_num(numbers, n):
+    answer = 0
+    for num in numbers:
+        answer += num
+        if answer > n:
+            return answer
 
+'''
+return next(sum(numbers[:i + 1]) for i in range(len(numbers)) if sum(numbers[:i + 1]) > n)
 
-print()
+from itertools import accumulate
+def find_first_exceeding_sum(numbers, n):
+    return next((s for s in accumulate(numbers) if s > n), None)
+'''
+
+print(add_num([34, 5, 71, 29, 100, 34],	123))   # 139
+print(add_num([58, 44, 27, 10, 100],	139))       # 239
 
 
 # 70) ====================================================================
+#  s ≤ i ≤ e인 모든 i에 대해 arr[i]에 1을 더합니다.
+
+def section_query(arr, queries):
+    for s, e in queries:
+        for i in range(s, e+1):
+            arr[i] += 1
+    return arr
+
+'''
+JS
+queries.forEach(([s, e]) => {
+    while (s <= e) arr[s++]++;
+});
+'''
 
 
-
-print()
+# [1, 3, 4, 4, 4]
+print(section_query([0, 1, 2, 3, 4],	[[0, 1],[1, 2],[2, 3]]))
 
